2b166c3198ed17b115bcd95b71c185ac69ffaf7d
Futa Hirakoba <futa-hirakoba@cybozu.co.jp>
2021-03-24 01:23:33 +0900

    ci: fix test

--- lib/tempura_test.go
+++ lib/tempura_test.go
@@ -45,7 +45,6 @@ func Test_Fill(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			tempura := Tempura{tt.args.template, "", tt.args.input}
 
 			got, err := tempura.Fill()
